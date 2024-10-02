4204f6aac46babd13000610f20f8901ea5768430
Samm Wiltshire <samm94tw@gmail.com>
2024-10-02 11:59:08 +0100

    enhancement: debug

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,13 +7,8 @@ const salt = parseInt(process.env.SALT);
 const hashPass = async (req, res, next) => {
     console.log("hashpass req.body", req.body)
     try {
-// hash password
 const hashedPass = await bcrypt.hash(req.body.password, salt);
-
-// replace password
 req.body.password = hashedPass;
-
-// use next()
 next();
 
     } catch (error) {
